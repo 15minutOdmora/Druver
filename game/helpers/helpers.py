45fef57395e792e57221e8b1782336df0cd8732f
15minutOdmora <liammislej@gmail.com>
2021-10-24 12:49:13 +0200

    Created helpers module, design start page and implemented redirection to web page

---
+++ game/helpers/helpers.py
@@ -0,0 +1,19 @@
+"""
+Module containing various helper functions that don't belong in specific files.
+Functions and or objects should be written in pure Python.
+"""
+
+
+def create_callable(func: Callable, *args, **kwargs):
+    """
+    Function creates a callable function where the called function gets passed as an argument along with its
+    args and kwargs.
+
+    :param func: Function to be called
+    :param args: Args passed to passed function
+    :param kwargs: Kwargs passed to passed function
+    :return: Callable: Function that executes passed function when called.
+    """
+    def callable_func():
+        return func(*args, **kwargs)
+    return callable_func
