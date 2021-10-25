171b7591c4a723ac5deb87a15005e74e733c9f0f
15minutOdmora <liammislej@gmail.com>
2021-10-25 21:52:33 +0200

    Implemented basic play package structure, map and player class along with tile loading and index updating.

--- game/helpers/file_handling.py
+++ game/helpers/file_handling.py
@@ -18,6 +18,21 @@ def abs_path(path: str) -> str:
     return os.path.abspath(path)
 
 
+def get_indexes_from_string(in_str: str) -> list:
+    """
+    Function converts strings like '001' into list [0, 0, 1]
+    :param in_str: Input string to convert
+    :return: tuple with converted values
+    """
+    num_list = []
+    for char in in_str:
+        try:
+            num_list.append(int(char))
+        except ValueError:
+            num_list.append(None)
+    return num_list
+
+
 class ImageLoader:
     @staticmethod
     def load_image(image_path: str) -> "Surface":
@@ -72,6 +87,25 @@ class ImageLoader:
             path = os.path.join(folder_path, image_path)
             image_list.append(ImageLoader.load_transparent_image(path))
         return image_list
+
+    @staticmethod
+    def load_tiles_from_folder(folder_path: str) -> list[list[tuple]]:
+        images = DirectoryReader.get_all_files(folder_path)
+        rows, columns = [], []
+        for name, path in images:
+            indexes = name.split(".")[0]  # Get left side of .
+            try:
+                rows.append(int(indexes[0]))
+                columns.append(int(indexes[1]))
+            except ValueError:
+                raise ValueError(f"Loading tiles format error, incorrect format for image: {name} in folder: {folder_path}")
+        # Create grid based on max values from rows and columns
+        grid = [[None for j in range(max(columns) + 1)] for i in range(max(rows) + 1)]  # Add 1 as indexing starts at 0
+        # Add images to grid
+        for name, path in images:
+            indexes = get_indexes_from_string(name.split(".")[0])
+            grid[indexes[0]][indexes[1]] = ImageLoader.load_image(path)
+        return grid
 
 
 class DirectoryReader:
--- game/play/game_objects/map.py
+++ game/play/game_objects/map.py
@@ -1,25 +1,167 @@
 """
 Module containing different maps.
 """
+from __future__ import annotations
+import math
+
+import pygame
+
+from game.constants import Paths, join_paths, SCREEN_SIZE
+from game.helpers.file_handling import DirectoryReader, ImageLoader
+
+
+def get_indexes(position: list[int], divisor: list[int]) -> list[int, int]:
+    """
+    Function calculates integer division of point and a divisor vector.
+    Ex. position = point, divisor = tile size => indexes [i, j] of tile in grid where the point lies.
+    :param position: list[int, int] position as list
+    :param divisor: list[int, int] divisor pair where [xd, yd] xd is divisor on x axis, yd on y axis
+    :return: list[int, int] indexes [i, j]
+    """
+    return [position[1] // divisor[1], position[0] // divisor[0]]
 
 
 class Tile:
-    def __init__(self):
-        pass
+    """
+    Tile class for representing a single tile on map.
+    """
+    def __init__(self, screen: "Display", image: "Image", position: list[int, int], size: list[int, int]):
+        """
+        :param screen: Currently opened display
+        :param image: Image, used as ground
+        :param position: Position of tile relative to top-left corner of map
+        :param size: Size of tile in [px, px]
+        """
+        self.screen = screen
+        self.image = image
+        self.position = position
+        self.size = size
 
     def update(self):
         pass
 
-    def draw(self):
-        pass
+    def draw(self, offset: list[int, int]) -> None:
+        """
+        Method draws tile on screen moved by passed offset.
+        :param offset: list[int, int] Offset to move tile
+        """
+        self.screen.blit(self.image, [self.position[0] - offset[0], self.position[1] - offset[1]])
 
 
 class Map:
-    def __init__(self):
-        pass
+    """
+    Class representing map.
 
-    def update(self):
-        pass
+    Attributes:
+        tiles: Grid containing all tiles
+        tile_size: Size of one tile
+        map_size: Size of map in px
+        offset: Offset to move map (based on player)
+    """
+    def __init__(self, game, folder_name):
+        """
+        :param game: Current game object
+        :param folder_name: Name of map folder saved in assets/maps/
+        """
+        self.game = game
+        self.controller = self.game.controller
+        self.screen = self.game.screen
+        self.screen_size = SCREEN_SIZE
+        self.half_screen_width, self.half_screen_height = SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 2
+        self.folder_name = folder_name
+        # These get set by the load_tiles method
+        self.tiles: list[list] = None
+        self.tile_size = None
+        self.number_of_tiles = None
+        self.map_size = None
+        self.offset = [0, 0]
+        # Currently visible tiles
+        self.visible_tiles = [[0, 0], [0, 1], [1, 0], [1, 1]]
+        # Load all tile data
+        self.__load_tiles()
 
-    def draw(self):
-        pass
+    def __load_tiles(self) -> None:
+        """
+        Method loads tiles into the tiles list grid.
+        """
+        folder_path = join_paths(Paths.maps, self.folder_name)
+        ground_folder_path = join_paths(folder_path, "ground")
+        # TODO: Implement for mask, update for different sized tiles
+        ground_images = ImageLoader.load_tiles_from_folder(ground_folder_path)  # Loads every image in a grid
+        self.tiles = []
+        self.tile_size = ground_images[0][0].get_size()  # Get size of one image
+        current_position = [0, 0]
+        for i in range(len(ground_images)):
+            self.tiles.append([])
+            current_position[0] = 0
+            for j in range(len(ground_images[i])):
+                image = ground_images[i][j]
+                # Initializing Tile position by passing current_position, does not work as the initialized position
+                # takes the last assigned value of current_position.
+                self.tiles[i].append(
+                    Tile(
+                        self.screen,
+                        image,
+                        [current_position[0], current_position[1]],
+                        self.tile_size
+                    )
+                )
+                current_position[0] += self.tile_size[0]
+            current_position[1] += self.tile_size[1]
+        self.map_size = (current_position[0], current_position[1])
+        self.number_of_tiles = [len(self.tiles), len(self.tiles[0])]
+
+    def update_visible_tiles_indexes(self) -> None:
+        """
+        Method updates all currently visible tiles.
+        """
+        # Empty list, and create temporary list
+        self.visible_tiles, visible_tiles = [], []
+        # Corners
+        top_left = get_indexes(
+            [self.offset[0] - self.half_screen_width, self.offset[1] - self.half_screen_height],
+            self.tile_size
+        )
+        top_right = get_indexes(
+            [self.offset[0] + self.half_screen_width, self.offset[1] - self.half_screen_height],
+            self.tile_size
+        )
+        bottom_right = get_indexes(
+            [self.offset[0] - self.half_screen_width, self.offset[1] + self.half_screen_height],
+            self.tile_size
+        )
+        bottom_left = get_indexes(
+            [self.offset[0] + self.half_screen_width, self.offset[1] + self.half_screen_height],
+            self.tile_size
+        )
+        # Create list
+        visible_tiles += [top_left, top_right, bottom_right, bottom_left]
+        for indexes in visible_tiles:
+            i, j = indexes[0], indexes[1]
+            if 0 <= i <= len(self.tiles) - 1:
+                if 0 <= j <= len(self.tiles[i]) - 1:
+                    self.visible_tiles.append(indexes)
+
+    def update(self) -> None:
+        """
+        Method updates tiles. (And currently also the offset)
+        """
+        self.update_visible_tiles_indexes()
+        if self.controller.key_pressed["right"]:
+            self.offset[0] += 3
+        if self.controller.key_pressed["left"]:
+            self.offset[0] -= 3
+        if self.controller.key_pressed["up"]:
+            self.offset[1] -= 3
+        if self.controller.key_pressed["down"]:
+            self.offset[1] += 3
+
+    def draw(self) -> None:
+        """
+        Method draws currently visible tiles on screen based on offset.
+        """
+        # Draw tiles on calculated positions, move them by offset and half screen size
+        offset = [self.offset[0] - self.half_screen_width, self.offset[1] - self.half_screen_height]
+        for indexes in self.visible_tiles:
+            i, j = indexes[0], indexes[1]
+            self.tiles[i][j].draw(offset)
