298c2b6a6d4e6b55cc475f24608b6e2de24bea26
15minutOdmora <liammislej@gmail.com>
2021-11-15 21:09:41 +0100

    Added and modified some docs.

--- game/development.py
+++ game/development.py
@@ -2,11 +2,20 @@
 Module containing development class for drawing stats to page.
 """
 
+from annotations import Callable
+
 import pygame
 
 
 class Development:
+    """
+    Class development used throughout the game, has method for adding functions that draw on screen.
+    Used for displaying data from various modules and classes.
+    """
     def __init__(self, game):
+        """
+        :param game: Game main game object containing all other main objects(window, input, controller, ...)
+        """
         self.game: "Game" = game
 
         self.screen = pygame.display.get_surface()
@@ -19,13 +28,13 @@ class Development:
         self._current_fps_index: int = 0
         self.avg_fps: int = 0
 
-        self.items: list["function"] = []
+        self.items: list[Callable] = []
 
         self.add(self.__draw_fps)  # Add the draw fps method to items
         self.add(self.__draw_page_stack)
         self.add(self.__draw_game_dt)
 
-    def __get_fps(self):
+    def __get_fps(self) -> None:
         """
         Method calculates the average fps of 200 consecutive frames.
         """
@@ -37,22 +46,31 @@ class Development:
         self._current_fps_index += 1
         self.avg_fps = round(avg_fps)
 
-    def __draw_fps(self):
+    def __draw_fps(self) -> None:
+        """
+        Method draws fps data on screen.
+        """
         fps_str = f"FPS: {self.avg_fps}"
         fps_surface = self.font.render(fps_str, True, (255, 255, 255))
         self.screen.blit(fps_surface, (20, 700))
 
-    def __draw_page_stack(self):
+    def __draw_page_stack(self) -> None:
+        """
+        Method draws number of pages currently in page_stack of controller.
+        """
         num_of_pages = f"Page stack:  {len(self.game.controller.page_stack)}"
         pages_surface = self.font.render(num_of_pages, True, (255, 255, 255))
         self.game.screen.blit(pages_surface, (20, 680))
 
-    def __draw_game_dt(self):
+    def __draw_game_dt(self) -> None:
+        """
+        Method draws the number of seconds passed from the previous frame on screen.
+        """
         dt = f"Last time diff {self.game.dt}"
         dt_surface = self.font.render(dt, True, (255, 255, 255))
         self.game.screen.blit(dt_surface, (20, 660))
 
-    def add(self, func: "function") -> None:
+    def add(self, func: Callable) -> None:
         """
         Method adds function to items, function then gets executed each loop.
         :param func: Function name -> callable
@@ -60,9 +78,15 @@ class Development:
         self.items.append(func)
 
     def update(self):
+        """
+        Method used for updating all data stored in self.
+        """
         self.__get_fps()
 
-    def draw(self):
+    def draw(self) -> None:
+        """
+        Method draws self if set to visible, it also calls all functions added to self.items.
+        """
         if self.visible:
             self.update()
             for item in self.items:
